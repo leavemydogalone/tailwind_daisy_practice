3e445e6d2c952f950444b4abaaa4d3b359e131e0
leavemydogalone <niklasport88@gmail.com>
2022-06-30 22:19:58 -0700

    wow this is fast

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Nav";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App flex justify-center items-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Im a button</button>
+    <div className="App">
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img
+            src="https://placeimg.com/260/400/arch"
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div className="lg:px-6">
+            <h1 class="text-5xl font-bold">Box Office News!</h1>
+            <p class="py-6 text-3xl ">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Nav />
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="account" element={<Account />} />
